d8e7666cd92c320955011e674bfa811b3dfda70d
srinathkv <srinath.crre@gmail.com>
2020-11-07 12:16:02 +0530

    first commit

---
+++ virtual-inheritance.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+	int a;
+	public:
+		Base()
+		{
+			a = 10;
+		}
+		void show()
+		{
+	                cout << a << endl;
+			cout << "In show" << endl;
+		}
+};
+class D1 : public  virtual Base
+{
+};
+class D2 : public  virtual Base
+{
+};
+class D11 : public D1, public D2
+{
+};
+
+int main()
+{
+	D11 d;
+	//cout << d.a << endl;
+	d.show();
+	return 0;
+}
